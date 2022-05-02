c5a21ac07b5a1a9117afb223732da156b9b2910d
huilang <yangyang1040553@gmail.com>
2022-05-02 18:52:20 +0700

    修改多语言

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -1,9 +1,11 @@
 <template>
   <el-breadcrumb class="app-breadcrumb" separator="/">
     <transition-group name="breadcrumb">
-      <el-breadcrumb-item v-for="(item,index) in levelList" :key="item.path">
-        <span v-if="item.redirect === 'noRedirect' || index == levelList.length - 1" class="no-redirect">{{ item.meta.title }}</span>
-        <a v-else @click.prevent="handleLink(item)">{{ item.meta.title }}</a>
+      <el-breadcrumb-item v-for="(item, index) in levelList" :key="item.path">
+        <span v-if="item.redirect === 'noRedirect' || index == levelList.length - 1" class="no-redirect">{{
+            $t(item.meta.title.toString())
+        }}</span>
+        <a v-else @click.prevent="handleLink(item)">{{ $t(item.meta.title.toString()) }}</a>
       </el-breadcrumb-item>
     </transition-group>
   </el-breadcrumb>
@@ -35,7 +37,7 @@ export default {
       const first = matched[0]
 
       if (!this.isDashboard(first)) {
-        matched = [{ path: '/index', meta: { title: $t('homePage') }}].concat(matched)
+        matched = [{ path: '/index', meta: { title: '' } }].concat(matched)
       }
 
       this.levelList = matched.filter(item => item.meta && item.meta.title && item.meta.breadcrumb !== false)
--- src/views/telegram/index.vue
+++ src/views/telegram/index.vue
@@ -1,117 +1,289 @@
 <template>
-    <div class="app-container">
-        <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" label-width="68px">
-            <el-form-item label="" prop="ipaddr">
-                <el-button type="primary" size="mini">{{ $t('new') }}</el-button>
-            </el-form-item>
-            <el-form-item :label="$t('status')" prop="ipaddr">
-                <el-select v-model="status" :placeholder="$t('select_status')" clearable>
-                    <el-option v-for="dict in statusArr" :key="dict.Value" :label="dict.Value" :value="dict.Key" />
-                </el-select>
-            </el-form-item>
-            <el-form-item label="" prop="userName">
-                <el-input v-model="queryParams.userName" :placeholder="$t('type_keyword')" clearable
-                    @keyup.enter.native="handleQuery" />
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">{{ $t('search') }}
-                </el-button>
-                <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">{{ $t('reset') }}</el-button>
-            </el-form-item>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
+      <el-form-item label="Token" prop="token">
+        <el-input
+          v-model="queryParams.token"
+          placeholder="请输入Token"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="飞机号" prop="userName">
+        <el-input
+          v-model="queryParams.userName"
+          placeholder="请输入飞机号"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="创建时间" prop="createTime">
+        <el-date-picker clearable
+          v-model="queryParams.createTime"
+          type="date"
+          value-format="yyyy-MM-dd"
+          placeholder="请选择创建时间">
+        </el-date-picker>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
 
-        </el-form>
-        <el-table v-loading="loading" :data="list.slice((pageNum - 1) * pageSize, pageNum * pageSize)"
-            style="width: 100%;">
-            <el-table-column label="序号" type="index" align="center">
-                <template slot-scope="scope">
-                    <span>{{ (pageNum - 1) * pageSize + scope.$index + 1 }}</span>
-                </template>
-            </el-table-column>
-            <el-table-column label="会话编号" align="center" prop="tokenId" :show-overflow-tooltip="true" />
-            <el-table-column label="登录名称" align="center" prop="userName" :show-overflow-tooltip="true" />
-            <el-table-column label="部门名称" align="center" prop="deptName" />
-            <el-table-column label="主机" align="center" prop="ipaddr" :show-overflow-tooltip="true" />
-            <el-table-column label="登录地点" align="center" prop="loginLocation" :show-overflow-tooltip="true" />
-            <el-table-column label="浏览器" align="center" prop="browser" />
-            <el-table-column label="操作系统" align="center" prop="os" />
-            <el-table-column label="登录时间" align="center" prop="loginTime" width="180">
-                <template slot-scope="scope">
-                    <span>{{ parseTime(scope.row.loginTime) }}</span>
-                </template>
-            </el-table-column>
-            <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
-                <template slot-scope="scope">
-                    <el-button size="mini" type="text" icon="el-icon-delete" @click="handleForceLogout(scope.row)"
-                        v-hasPermi="['monitor:online:forceLogout']">强退</el-button>
-                </template>
-            </el-table-column>
-        </el-table>
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+          type="primary"
+          plain
+          icon="el-icon-plus"
+          size="mini"
+          @click="handleAdd"
+          v-hasPermi="['telegram:bot:add']"
+        >新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="success"
+          plain
+          icon="el-icon-edit"
+          size="mini"
+          :disabled="single"
+          @click="handleUpdate"
+          v-hasPermi="['telegram:bot:edit']"
+        >修改</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="danger"
+          plain
+          icon="el-icon-delete"
+          size="mini"
+          :disabled="multiple"
+          @click="handleDelete"
+          v-hasPermi="['telegram:bot:remove']"
+        >删除</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="warning"
+          plain
+          icon="el-icon-download"
+          size="mini"
+          @click="handleExport"
+          v-hasPermi="['telegram:bot:export']"
+        >导出</el-button>
+      </el-col>
+      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
 
-        <pagination v-show="total > 0" :total="total" :page.sync="pageNum" :limit.sync="pageSize" />
-    </div>
+    <el-table v-loading="loading" :data="botList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="Id" align="center" prop="id" />
+      <el-table-column label="Token" align="center" prop="token" />
+      <el-table-column label="飞机号" align="center" prop="userName" />
+      <el-table-column label="描述" align="center" prop="description" />
+      <el-table-column label="创建时间" align="center" prop="createTime" width="180">
+        <template slot-scope="scope">
+          <span>{{ parseTime(scope.row.createTime, '{y}-{m}-{d}') }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="更新时间" align="center" prop="updateTime" width="180">
+        <template slot-scope="scope">
+          <span>{{ parseTime(scope.row.updateTime, '{y}-{m}-{d}') }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="创建者" align="center" prop="createBy" />
+      <el-table-column label="更新者" align="center" prop="updateBy" />
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-edit"
+            @click="handleUpdate(scope.row)"
+            v-hasPermi="['telegram:bot:edit']"
+          >修改</el-button>
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-delete"
+            @click="handleDelete(scope.row)"
+            v-hasPermi="['telegram:bot:remove']"
+          >删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
+
+    <!-- 添加或修改机器人对话框 -->
+    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="Token" prop="token">
+          <el-input v-model="form.token" placeholder="请输入Token" />
+        </el-form-item>
+        <el-form-item label="飞机号" prop="userName">
+          <el-input v-model="form.userName" placeholder="请输入飞机号" />
+        </el-form-item>
+        <el-form-item label="描述" prop="description">
+          <el-input v-model="form.description" placeholder="请输入描述" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
-import { list, forceLogout } from "@/api/monitor/online";
+import { listBot, getBot, delBot, addBot, updateBot } from "@/api/telegram/bot";
 
 export default {
-    name: "Online",
-    data() {
-        return {
-            statusArr: [
-                { Value: "全部", Key: "999" },
-                { Value: "启用", Key: "1" },
-                { Value: "禁用", Key: "2" }
-            ],
-            status: "999",
-            // 遮罩层
-            loading: true,
-            // 总条数
-            total: 0,
-            // 表格数据
-            list: [],
-            pageNum: 1,
-            pageSize: 10,
-            // 查询参数
-            queryParams: {
-                ipaddr: undefined,
-                userName: undefined
-            }
-        };
+  name: "Bot",
+  data() {
+    return {
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 显示搜索条件
+      showSearch: true,
+      // 总条数
+      total: 0,
+      // 机器人表格数据
+      botList: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        token: null,
+        userName: null,
+        createTime: null,
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+      }
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    /** 查询机器人列表 */
+    getList() {
+      this.loading = true;
+      listBot(this.queryParams).then(response => {
+        this.botList = response.rows;
+        this.total = response.total;
+        this.loading = false;
+      });
     },
-    created() {
-        this.getList();
+    // 取消按钮
+    cancel() {
+      this.open = false;
+      this.reset();
     },
-    methods: {
-        /** 查询登录日志列表 */
-        getList() {
-            this.loading = true;
-            list(this.queryParams).then(response => {
-                this.list = response.rows;
-                this.total = response.total;
-                this.loading = false;
+    // 表单重置
+    reset() {
+      this.form = {
+        id: null,
+        token: null,
+        userName: null,
+        description: null,
+        createTime: null,
+        updateTime: null,
+        createBy: null,
+        updateBy: null
+      };
+      this.resetForm("form");
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    // 多选框选中数据
+    handleSelectionChange(selection) {
+      this.ids = selection.map(item => item.id)
+      this.single = selection.length!==1
+      this.multiple = !selection.length
+    },
+    /** 新增按钮操作 */
+    handleAdd() {
+      this.reset();
+      this.open = true;
+      this.title = "添加机器人";
+    },
+    /** 修改按钮操作 */
+    handleUpdate(row) {
+      this.reset();
+      const id = row.id || this.ids
+      getBot(id).then(response => {
+        this.form = response.data;
+        this.open = true;
+        this.title = "修改机器人";
+      });
+    },
+    /** 提交按钮 */
+    submitForm() {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          if (this.form.id != null) {
+            updateBot(this.form).then(response => {
+              this.$modal.msgSuccess("修改成功");
+              this.open = false;
+              this.getList();
             });
-        },
-        /** 搜索按钮操作 */
-        handleQuery() {
-            this.pageNum = 1;
-            this.getList();
-        },
-        /** 重置按钮操作 */
-        resetQuery() {
-            this.resetForm("queryForm");
-            this.handleQuery();
-        },
-        /** 强退按钮操作 */
-        handleForceLogout(row) {
-            this.$modal.confirm('是否确认强退名称为"' + row.userName + '"的用户？').then(function () {
-                return forceLogout(row.tokenId);
-            }).then(() => {
-                this.getList();
-                this.$modal.msgSuccess("强退成功");
-            }).catch(() => { });
+          } else {
+            addBot(this.form).then(response => {
+              this.$modal.msgSuccess("新增成功");
+              this.open = false;
+              this.getList();
+            });
+          }
         }
+      });
+    },
+    /** 删除按钮操作 */
+    handleDelete(row) {
+      const ids = row.id || this.ids;
+      this.$modal.confirm('是否确认删除机器人编号为"' + ids + '"的数据项？').then(function() {
+        return delBot(ids);
+      }).then(() => {
+        this.getList();
+        this.$modal.msgSuccess("删除成功");
+      }).catch(() => {});
+    },
+    /** 导出按钮操作 */
+    handleExport() {
+      this.download('telegram/bot/export', {
+        ...this.queryParams
+      }, `bot_${new Date().getTime()}.xlsx`)
     }
+  }
 };
 </script>
-
