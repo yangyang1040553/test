2807f82aae701455fe5b8e6f1d435ee3fff7bd28
huilang <yangyang1040553@gmail.com>
2022-05-10 20:01:37 +0700

    去除无用代码

--- src/views/login.vue
+++ src/views/login.vue
@@ -33,9 +33,9 @@
       </el-form-item>
     </el-form>
     <!--  底部  -->
-    <div class="el-login-footer">
+    <!-- <div class="el-login-footer">
       <span>Copyright © 2018-2019 ruoyi.vip All Rights Reserved.</span>
-    </div>
+    </div> -->
   </div>
 </template>
 
--- src/views/register.vue
+++ src/views/register.vue
@@ -60,9 +60,9 @@
       </el-form-item>
     </el-form>
     <!--  底部  -->
-    <div class="el-register-footer">
+    <!-- <div class="el-register-footer">
       <span>Copyright © 2018-2022 ruoyi.vip All Rights Reserved.</span>
-    </div>
+    </div> -->
   </div>
 </template>
 
