6d6d8469be6df25901f846c26e813ed663f16ab0
huilang <yangyang1040553@gmail.com>
2022-05-11 13:55:23 +0700

    修改路径端口

--- vue.config.js
+++ vue.config.js
@@ -37,7 +37,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://192.168.0.84:8022`,
+        target: `http://192.168.0.23:8022`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
