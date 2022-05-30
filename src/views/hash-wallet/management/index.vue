e786eedf80a05fae1e6f2f0f945a6e2dc4b87986
huilang <yangyang1040553@gmail.com>
2022-05-30 12:11:19 +0700

    用户钱包 添加用户详情

--- src/views/hash-wallet/management/index.vue
+++ src/views/hash-wallet/management/index.vue
@@ -1,31 +1,68 @@
 <template>
   <div class="app-container">
-    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="88px">
+    <el-form
+      :model="queryParams"
+      ref="queryForm"
+      size="small"
+      :inline="true"
+      v-show="showSearch"
+      label-width="88px"
+    >
       <el-form-item label="账单ID" prop="id">
-        <el-input v-model="queryParams.id" placeholder="请输入账单ID" clearable @keyup.enter.native="handleQuery" />
+        <el-input
+          v-model="queryParams.id"
+          placeholder="请输入账单ID"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
       </el-form-item>
       <el-form-item label="usdt余额" prop="usdtAmount">
-        <el-input v-model="queryParams.usdtAmount" placeholder="请输入usdt余额" clearable
-          @keyup.enter.native="handleQuery" />
+        <el-input
+          v-model="queryParams.usdtAmount"
+          placeholder="请输入usdt余额"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
       </el-form-item>
       <el-form-item label="trx余额" prop="trxAmount">
-        <el-input v-model="queryParams.trxAmount" placeholder="请输入trx余额" clearable @keyup.enter.native="handleQuery" />
+        <el-input
+          v-model="queryParams.trxAmount"
+          placeholder="请输入trx余额"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
       </el-form-item>
       <el-form-item label="Usdt地址" prop="hashAddressUsdt">
-        <el-input v-model="queryParams.hashAddressUsdt" placeholder="请输入Usdt的hash地址" clearable
-          @keyup.enter.native="handleQuery" />
+        <el-input
+          v-model="queryParams.hashAddressUsdt"
+          placeholder="请输入Usdt的hash地址"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
       </el-form-item>
       <el-form-item label="Trx地址" prop="hashAddressTrx">
-        <el-input v-model="queryParams.hashAddressTrx" placeholder="请输入Trx的hash地址" clearable
-          @keyup.enter.native="handleQuery" />
+        <el-input
+          v-model="queryParams.hashAddressTrx"
+          placeholder="请输入Trx的hash地址"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
       </el-form-item>
       <el-form-item label="总充值金额" prop="rechargeTotal">
-        <el-input v-model="queryParams.rechargeTotal" placeholder="请输入总充值金额" clearable
-          @keyup.enter.native="handleQuery" />
+        <el-input
+          v-model="queryParams.rechargeTotal"
+          placeholder="请输入总充值金额"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
       </el-form-item>
       <el-form-item label="总提现金额" prop="withdrawTotal">
-        <el-input v-model="queryParams.withdrawTotal" placeholder="请输入总提现金额" clearable
-          @keyup.enter.native="handleQuery" />
+        <el-input
+          v-model="queryParams.withdrawTotal"
+          placeholder="请输入总提现金额"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
@@ -37,25 +74,48 @@
       <!-- <el-col :span="1.5">
         <el-button type="primary" plain icon="el-icon-plus" size="mini" @click="handleAdd"
           v-hasPermi="['hash-wallet:management:add']">新增</el-button>
-      </el-col> -->
+      </el-col>-->
       <el-col :span="1.5">
-        <el-button type="success" plain icon="el-icon-edit" size="mini" :disabled="single" @click="handleUpdate"
-          v-hasPermi="['hash-wallet:management:edit']">修改</el-button>
+        <el-button
+          type="success"
+          plain
+          icon="el-icon-edit"
+          size="mini"
+          :disabled="single"
+          @click="handleUpdate"
+          v-hasPermi="['hash-wallet:management:edit']"
+        >修改</el-button>
       </el-col>
       <!-- <el-col :span="1.5">
         <el-button type="danger" plain icon="el-icon-delete" size="mini" :disabled="multiple" @click="handleDelete"
           v-hasPermi="['hash-wallet:management:remove']">删除</el-button>
-      </el-col> -->
+      </el-col>-->
       <el-col :span="1.5">
-        <el-button type="warning" plain icon="el-icon-download" size="mini" @click="handleExport"
-          v-hasPermi="['hash-wallet:management:export']">导出</el-button>
+        <el-button
+          type="warning"
+          plain
+          icon="el-icon-download"
+          size="mini"
+          @click="handleExport"
+          v-hasPermi="['hash-wallet:management:export']"
+        >导出</el-button>
       </el-col>
       <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
-    <el-table v-loading="loading" :data="managementList" @selection-change="handleSelectionChange" height="600">
+    <el-table
+      v-loading="loading"
+      :data="managementList"
+      @selection-change="handleSelectionChange"
+      height="600"
+    >
       <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="ID" align="center" prop="id" />
+      <!-- <el-table-column label="ID" align="center" prop="id" /> -->
+      <el-table-column label="ID" align="center" prop="id">
+        <template slot-scope="scope">
+          <div class="global-text-blue" @click="openUserDetail(scope.row)">{{scope.row.id}}</div>
+        </template>
+      </el-table-column>
       <el-table-column label="usdt余额" align="center" prop="usdtAmount" />
       <el-table-column label="trx余额" align="center" prop="trxAmount" />
       <el-table-column label="Usdt的hash地址" align="center" prop="hashAddressUsdt" />
@@ -64,16 +124,26 @@
       <el-table-column label="总提现金额" align="center" prop="withdrawTotal" />
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template slot-scope="scope">
-          <el-button size="mini" type="text" icon="el-icon-detail" @click="handleUpdate(scope.row)"
-            v-hasPermi="['hash-wallet:management:edit']">详情</el-button>
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-detail"
+            @click="handleUpdate(scope.row)"
+            v-hasPermi="['hash-wallet:management:edit']"
+          >详情</el-button>
           <!-- <el-button size="mini" type="text" icon="el-icon-delete" @click="handleDelete(scope.row)"
-            v-hasPermi="['hash-wallet:management:remove']">删除</el-button> -->
+          v-hasPermi="['hash-wallet:management:remove']">删除</el-button>-->
         </template>
       </el-table-column>
     </el-table>
 
-    <pagination v-show="total > 0" :total="total" :page.sync="queryParams.pageNum" :limit.sync="queryParams.pageSize"
-      @pagination="getList" />
+    <pagination
+      v-show="total > 0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
 
     <!-- 添加或修改用户钱包对话框 -->
     <el-dialog :title="title" :visible.sync="open" width="800px" append-to-body>
@@ -101,16 +171,23 @@
         <el-button @click="cancel">关 闭</el-button>
       </div>
     </el-dialog>
+
+    <UserInfoDialog v-if="openUser" :open="openUser" :id="userId" @close="openUser=false" />
   </div>
 </template>
 
 <script>
 import { listManagement, getManagement, delManagement, addManagement, updateManagement } from "@/api/hash-wallet/management";
-
+import UserInfoDialog from "../../components/dialog/UserInfoDialog.vue";
 export default {
   name: "Management",
+  components: {
+    UserInfoDialog
+  },
   data() {
     return {
+      openUser: false,
+      userId: null,
       // 遮罩层
       loading: true,
       // 选中数组
@@ -152,6 +229,10 @@ export default {
     this.getList();
   },
   methods: {
+    openUserDetail(row) {
+      this.openUser = true;
+      this.userId = row.id;
+    },
     /** 查询用户钱包列表 */
     getList() {
       this.loading = true;
