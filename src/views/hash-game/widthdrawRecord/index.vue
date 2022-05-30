6a57a816733c8500c92285af8e5b07c916a48a25
huilang <yangyang1040553@gmail.com>
2022-05-30 12:30:54 +0700

    佣金提现添加用户详情

--- src/views/hash-game/widthdrawRecord/index.vue
+++ src/views/hash-game/widthdrawRecord/index.vue
@@ -1,23 +1,65 @@
 <template>
   <div class="app-container">
-    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="88px">
-      <el-form-item label="ID" prop="id"><el-input v-model="queryParams.id" placeholder="请输入id" clearable @keyup.enter.native="handleQuery" /></el-form-item>
-      <el-form-item label="玩家id" prop="userId"><el-input v-model="queryParams.userId" placeholder="请输入玩家id" clearable @keyup.enter.native="handleQuery" /></el-form-item>
+    <el-form
+      :model="queryParams"
+      ref="queryForm"
+      size="small"
+      :inline="true"
+      v-show="showSearch"
+      label-width="88px"
+    >
+      <el-form-item label="ID" prop="id">
+        <el-input
+          v-model="queryParams.id"
+          placeholder="请输入id"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="玩家id" prop="userId">
+        <el-input
+          v-model="queryParams.userId"
+          placeholder="请输入玩家id"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
       <el-form-item label="提现金额" prop="withdrawAmount">
-        <el-input v-model="queryParams.withdrawAmount" placeholder="请输入提现金额" clearable @keyup.enter.native="handleQuery" />
+        <el-input
+          v-model="queryParams.withdrawAmount"
+          placeholder="请输入提现金额"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
       </el-form-item>
       <el-form-item label="钱包类型" prop="walletType">
         <el-select v-model="queryParams.walletType" placeholder="请选择钱包类型" clearable>
-          <el-option v-for="dict in dict.type.wallet_type" :key="dict.value" :label="dict.label" :value="dict.value" />
+          <el-option
+            v-for="dict in dict.type.wallet_type"
+            :key="dict.value"
+            :label="dict.label"
+            :value="dict.value"
+          />
         </el-select>
       </el-form-item>
       <el-form-item label="提现状态" prop="status">
         <el-select v-model="queryParams.status" placeholder="请选择提现状态" clearable>
-          <el-option v-for="dict in dict.type.withdraw_status" :key="dict.value" :label="dict.label" :value="dict.value" />
+          <el-option
+            v-for="dict in dict.type.withdraw_status"
+            :key="dict.value"
+            :label="dict.label"
+            :value="dict.value"
+          />
         </el-select>
       </el-form-item>
       <el-form-item label="创建时间" prop="createTime">
-        <el-date-picker clearable v-model="queryParams.createTime" type="date" value-format="yyyy-MM-dd HH:mm:ss" placeholder="请选择创建时间"></el-date-picker>
+        <el-date-picker
+          clearable
+          v-model="queryParams.createTime"
+          type="date"
+          value-format="yyyy-MM-dd HH:mm:ss"
+          placeholder="请选择创建时间"
+        ></el-date-picker>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
@@ -26,7 +68,7 @@
     </el-form>
 
     <el-row :gutter="10" class="mb8">
-    <!--  <el-col :span="1.5">
+      <!--  <el-col :span="1.5">
         <el-button type="primary" plain icon="el-icon-plus" size="mini" @click="handleAdd" v-hasPermi="['hash-wallet:widthdrawRecord:add']">新增</el-button>
       </el-col>
       <el-col :span="1.5">
@@ -36,17 +78,38 @@
         <el-button type="danger" plain icon="el-icon-delete" size="mini" :disabled="multiple" @click="handleDelete" v-hasPermi="['hash-wallet:widthdrawRecord:remove']">
           删除
         </el-button>
-      </el-col> -->
+      </el-col>-->
       <el-col :span="1.5">
-        <el-button type="warning" plain icon="el-icon-download" size="mini" @click="handleExport" v-hasPermi="['hash-wallet:widthdrawRecord:export']">导出</el-button>
+        <el-button
+          type="warning"
+          plain
+          icon="el-icon-download"
+          size="mini"
+          @click="handleExport"
+          v-hasPermi="['hash-wallet:widthdrawRecord:export']"
+        >导出</el-button>
       </el-col>
       <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
-    <el-table v-loading="loading" @sort-change="sortChange" :data="widthdrawRecordList" @selection-change="handleSelectionChange" height="600">
+    <el-table
+      v-loading="loading"
+      @sort-change="sortChange"
+      :data="widthdrawRecordList"
+      @selection-change="handleSelectionChange"
+      height="600"
+    >
       <el-table-column type="selection" width="55" align="center" />
       <el-table-column label="ID" align="center" prop="id" />
-      <el-table-column label="玩家id" align="center" prop="userId" />
+      <!-- <el-table-column label="玩家id" align="center" prop="userId" /> -->
+      <el-table-column label="玩家id" align="center" prop="userId" width="200">
+        <template slot-scope="scope">
+          <div
+            class="global-text-blue"
+            @click="openUserDetail(scope.row.userId)"
+          >{{scope.row.userId}}</div>
+        </template>
+      </el-table-column>
       <el-table-column label="提现金额" align="center" prop="withdrawAmount" sortable>
         <template slot-scope="scope">
           <div>{{ scope.row.withdrawAmount.toFixed(2) }}</div>
@@ -77,25 +140,47 @@
           <el-button size="mini" type="text" icon="el-icon-edit" @click="handleUpdate(scope.row)" v-hasPermi="['hash-wallet:widthdrawRecord:edit']">修改</el-button>
           <el-button size="mini" type="text" icon="el-icon-delete" @click="handleDelete(scope.row)" v-hasPermi="['hash-wallet:widthdrawRecord:remove']">删除</el-button>
         </template>
-      </el-table-column> -->
+      </el-table-column>-->
     </el-table>
 
-    <pagination v-show="total > 0" :total="total" :page.sync="queryParams.pageNum" :limit.sync="queryParams.pageSize" @pagination="getList" />
+    <pagination
+      v-show="total > 0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
 
     <!-- 添加或修改佣金提现记录对话框 -->
     <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
       <el-form ref="form" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="玩家id" prop="userId"><el-input v-model="form.userId" placeholder="请输入玩家id" /></el-form-item>
-        <el-form-item label="提现金额" prop="withdrawAmount"><el-input v-model="form.withdrawAmount" placeholder="请输入提现金额" /></el-form-item>
+        <el-form-item label="玩家id" prop="userId">
+          <el-input v-model="form.userId" placeholder="请输入玩家id" />
+        </el-form-item>
+        <el-form-item label="提现金额" prop="withdrawAmount">
+          <el-input v-model="form.withdrawAmount" placeholder="请输入提现金额" />
+        </el-form-item>
         <el-form-item label="钱包类型" prop="walletType">
           <el-select v-model="form.walletType" placeholder="请选择钱包类型">
-            <el-option v-for="dict in dict.type.wallet_type" :key="dict.value" :label="dict.label" :value="dict.value"></el-option>
+            <el-option
+              v-for="dict in dict.type.wallet_type"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value"
+            ></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="提现后剩余金额" prop="afterAmount"><el-input v-model="form.afterAmount" placeholder="请输入提现后剩余金额" /></el-form-item>
+        <el-form-item label="提现后剩余金额" prop="afterAmount">
+          <el-input v-model="form.afterAmount" placeholder="请输入提现后剩余金额" />
+        </el-form-item>
         <el-form-item label="提现状态 1成功 2转入钱包异常" prop="status">
           <el-select v-model="form.status" placeholder="请选择提现状态 1成功 2转入钱包异常">
-            <el-option v-for="dict in dict.type.withdraw_status" :key="dict.value" :label="dict.label" :value="parseInt(dict.value)"></el-option>
+            <el-option
+              v-for="dict in dict.type.withdraw_status"
+              :key="dict.value"
+              :label="dict.label"
+              :value="parseInt(dict.value)"
+            ></el-option>
           </el-select>
         </el-form-item>
       </el-form>
@@ -104,17 +189,23 @@
         <el-button @click="cancel">取 消</el-button>
       </div>
     </el-dialog>
+    <UserInfoDialog v-if="openUser" :open="openUser" :id="userId" @close="openUser=false" />
   </div>
 </template>
 
 <script>
 import { listWidthdrawRecord, getWidthdrawRecord, delWidthdrawRecord, addWidthdrawRecord, updateWidthdrawRecord } from '@/api/hash-game/widthdrawRecord'
-
+import UserInfoDialog from "../../components/dialog/UserInfoDialog.vue";
 export default {
   name: 'WidthdrawRecord',
   dicts: ['wallet_type', 'withdraw_status'],
+  components: {
+    UserInfoDialog
+  },
   data() {
     return {
+      openUser: false,
+      userId: null,
       // 遮罩层
       loading: true,
       // 选中数组
@@ -156,6 +247,10 @@ export default {
     this.getList()
   },
   methods: {
+    openUserDetail(userId) {
+      this.openUser = true;
+      this.userId = userId;
+    },
     sortChange(val) {
       console.log(val)
       if (val.order && val.order == 'descending') {
@@ -253,14 +348,14 @@ export default {
       const ids = row.id || this.ids
       this.$modal
         .confirm('是否确认删除佣金提现记录编号为"' + ids + '"的数据项？')
-        .then(function() {
+        .then(function () {
           return delWidthdrawRecord(ids)
         })
         .then(() => {
           this.getList()
           this.$modal.msgSuccess('删除成功')
         })
-        .catch(() => {})
+        .catch(() => { })
     },
     /** 导出按钮操作 */
     handleExport() {
