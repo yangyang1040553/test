cf8748ea7499b47d004bde4f94f8d3b800c6c909
huilang <yangyang1040553@gmail.com>
2022-06-10 13:21:03 +0700

    修改 上传

--- src/views/components/upload/Upload.vue
+++ src/views/components/upload/Upload.vue
@@ -49,7 +49,7 @@ export default {
       const isLt2M = file.size / 1024 / 1024 < 2;
 
       if (!isJPG) {
-        this.$message.error('上传头像图片只能是 JPG 格式!');
+        this.$message.error('上传头像图片只能是 JPG/PNG/WEBP 格式!');
       }
       if (!isLt2M) {
         this.$message.error('上传头像图片大小不能超过 2MB!');
